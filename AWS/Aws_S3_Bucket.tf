586be3574d33daf73d400d6d2435c74b2c726779
ravicwp <109152895+ravicwp@users.noreply.github.com>
2022-12-02 14:23:51 +0530

    Create Aws_S3_Bucket.tf

--- AWS/Aws_S3_Bucket.tf
+++ AWS/Aws_S3_Bucket.tf
@@ -14,13 +14,4 @@ resource "aws_s3_bucket" "bucket2" {
   }
 }
 
-resource "aws_s3_bucket" "bucket3" {
-  bucket = "examplebuckettftest"
-  acl    = "private"
-  versioning {
-    enabled = false
-    mfa_delete = false
-  }
-}
-
 
