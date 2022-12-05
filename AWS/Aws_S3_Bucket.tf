570f9c554dacfa020546cf35781e408136c6fe4f
ravicwp <109152895+ravicwp@users.noreply.github.com>
2022-12-05 14:54:08 +0530

    Update Aws_S3_Bucket.tf

--- AWS/Aws_S3_Bucket.tf
+++ AWS/Aws_S3_Bucket.tf
@@ -13,14 +13,3 @@ resource "aws_s3_bucket" "bucket2" {
     mfa_delete = false
   }
 }
-
-
-
-resource "aws_s3_bucket" "bucket4" {
-  bucket = "examplebuckettftest"
-  acl    = "private"
-  versioning {
-    enabled = false
-    mfa_delete = false
-  }
-}
